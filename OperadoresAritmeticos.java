5bd0bc7ffcacd1191f5408cb0347333a7e5a4660
Gilmar Alberto <gilabreu00@gmail.com>
2023-08-24 16:47:01 -0300

    java guanabara

---
+++ OperadoresAritmeticos.java
@@ -0,0 +1,15 @@
+public class OperadoresAritmeticos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1 = 3;
+        int n2 = 5;
+        float m = (n1 + n2)/2;
+        
+        System.out.println("A média é igual a: " + m);
+    }
+
+}
