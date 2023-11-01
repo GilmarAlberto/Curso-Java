0173d09cdcb836590b1ab0b7bb0e9781dd6984be
Gilmar Alberto <gilabreu00@gmail.com>
2023-11-01 10:39:41 -0300

    Curso Java Loiane

--- Desafios/Desafio001.java
+++ Desafios/Desafio001.java
@@ -1,4 +1,5 @@
 package Desafios;
+
 public class Desafio001 {
     public static void main(String[] args){
         System.out.println("Olá Mundo");
---
+++ Desafios/Desafio002.java
@@ -0,0 +1,7 @@
+package Desafios;
+
+public class Desafio002 {
+    public static void main(String[] args){
+        System.out.println("Olá Mundo " + args[0]);
+    }
+}
